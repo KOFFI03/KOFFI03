25a21eb4aeec5bb419ad183d94230fe282c66037
KFC <KFC@gmail.com>
2024-10-20 00:46:31 +0000

    fichier KFC

---
+++ BusibessCard-main/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -0,0 +1,114 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.businesscard.R
+import com.example.businesscard.ui.theme.BusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BusinessCardTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    BusinessCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BusinessCard() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Section de l'image du logo
+        Image(
+            painter = painterResource(id = R.drawable.android_logo),  // Remplace par ton image
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(100.dp)
+                .padding(bottom = 16.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        // Section pour le nom et le titre
+        Text(
+            text = "Amian Attobra Charles",
+            style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = "Developpeur d'application",
+            style = MaterialTheme.typography.bodyLarge.copy(
+                color = Color.Gray
+            ),
+            modifier = Modifier.padding(bottom = 32.dp)
+        )
+
+        // Section des informations de contact
+        ContactInfo(icon = R.drawable.ic_phone, contactText = "+225 07 68 63 29 88")
+        ContactInfo(icon = R.drawable.ic_social_media, contactText = "@jeancharlesamian15")
+        ContactInfo(icon = R.drawable.ic_email, contactText = "jeancharlesamian15@gmail.com")
+    }
+}
+
+@Composable
+fun ContactInfo(icon: Int, contactText: String) {
+    Row(
+        modifier = Modifier
+            .width(200.dp)
+            //.align(Alignment.CenterVertically)
+            .padding(8.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+
+    ) {
+        // Icône pour l'information
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        // Texte de contact
+        Text(text = contactText, style = MaterialTheme.typography.bodyMedium)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewBusinessCard() {
+    BusinessCardTheme {
+        BusinessCard()
+    }
+}
